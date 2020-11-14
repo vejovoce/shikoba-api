ce5c25c43ed48c88aa33cb8f1c740db171a4363e
Leonardo Leite <leonardo.leitesc@gmail.com>
2020-11-14 19:54:27 -0300

    Improve tests for the Accounts context and the user mutations based on the new fields

--- test/shikoba_web/graphql/accounts/user_schema/mutation_create_unverified_user_test.exs
+++ test/shikoba_web/graphql/accounts/user_schema/mutation_create_unverified_user_test.exs
@@ -5,36 +5,51 @@ defmodule ShikobaWeb.Graphql.Accounts.UserSchema.MutationCreateUnverifiedUserTes
 
   describe "when params is valid" do
     test "should return token and user", %{conn: conn} do
+      phones = ["123456789", "+5512344567789"]
+
+      params = %{
+        phones: phones,
+        email: "email@email.com",
+        password: @password,
+        birth: "2020-11-14",
+      }
+
       assert %{
         "data" => %{
           "createUnverifiedUser" => token
         }
-      } = post_query(conn, mutation_create_unverified_user(), %{email: "email@email.com", password: @password})
+      } = post_query(conn, mutation_create_unverified_user(), params)
       assert is_binary(token)
     end
   end
 
   describe "when params is invalid" do
     test "should return error if email is empty", %{conn: conn} do
+      params = %{email: "", password: @password, birth: "2020-11-14"}
+
       assert error_response("email can't be blank", "createUnverifiedUser") ==
-        post_query(conn, mutation_create_unverified_user(), %{email: "", password: @password})
+        post_query(conn, mutation_create_unverified_user(), params)
     end
 
     test "should return error if password is empty", %{conn: conn} do
+      params = %{email: "email@email.com", password: "", birth: "2020-11-14"}
+
       assert error_response("password can't be blank", "createUnverifiedUser") ==
-        post_query(conn, mutation_create_unverified_user(), %{email: "email@email.com", password: ""})
+        post_query(conn, mutation_create_unverified_user(), params)
     end
 
     test "should return error if email is invalid", %{conn: conn} do
+      params = %{email: "invalid email", password: @password, birth: "2020-11-14"}
+
       assert error_response("email has invalid format", "createUnverifiedUser") ==
-        post_query(conn, mutation_create_unverified_user(), %{email: "invalid email", password: @password})
+        post_query(conn, mutation_create_unverified_user(), params)
     end
   end
 
   defp mutation_create_unverified_user do
     """
-    mutation($email: String!, $password: String!) {
-      createUnverifiedUser(email: $email, password: $password)
+    mutation($email: String!, $password: String!, $birth: Date!) {
+      createUnverifiedUser(email: $email, password: $password, dateOfBirth: $birth)
     }
     """
   end
